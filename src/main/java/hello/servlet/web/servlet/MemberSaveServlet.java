73d78d6827fd617ea4250b612c0cba4889ba28e0
ratellll <latellbin@naver.com>
2023-03-27 23:01:20 +0900

    jsp mvc패턴 확인
     //다른 서블릿이나  jsp로 이동할수있는기능 서버내부에서 다시 호출이 발생한다
            dispatcher.forward(request, response);
    
            //WEB-INF는 무조건 컨트롤러를 거쳐야지 접속가능하다

--- src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
+++ src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
@@ -18,8 +18,6 @@ public class MemberSaveServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
 
---
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -0,0 +1,26 @@
+package hello.servlet.web.servletmvc;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(name = "MvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form.jsp")
+public class MvcMemberFormServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String viewPath = "/WEB-INF/views/new-form.jsp.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        //다른 서블릿이나  jsp로 이동할수있는기능 서버내부에서 다시 호출이 발생한다
+        dispatcher.forward(request, response);
+
+
+        //WEB-INF는 무조건 컨트롤러를 거쳐야지 접속가능하다
+    }
+}
