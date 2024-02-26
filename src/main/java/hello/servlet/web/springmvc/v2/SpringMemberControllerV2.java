ef83f29c4501486fb83b47cfaab5b84920d9fada
ratellll <latellbin@naver.com>
2024-02-26 23:22:20 +0900

    feat : requstmapping으로 중복되는 url 설정해주면 코드가 간략해짐

--- src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
+++ src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -3,12 +3,12 @@ package hello.servlet.web.springmvc.v2;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller //연관성이 있는 컨트롤러끼리만 묶어서 한클래스에 넣어두는것도 좋은것
@@ -38,8 +38,10 @@ public class SpringMemberControllerV2 {
 
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
+        int size= Integer.parseInt(request.getParameter("size"));
+        int account = Integer.parseInt(request.getParameter("account"));
 
-        Member member = new Member(username, age);
+        Member member = new Member(username, age,size,account);
         memberRepository.save(member);
 
         ModelAndView mv = new ModelAndView("save-result");
